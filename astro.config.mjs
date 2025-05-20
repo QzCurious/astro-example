cf770ac8b89cc7744c0e04741b8f7cf2093fcddd
Qz <qzcurious@gmail.com>
2025-05-20 12:35:28 +0800

    just-validate

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   // integrations: [relativeLinks()],
-  compressHTML: false,
   vite: {
     plugins: [tailwindcss()],
-    build: {
-      minify: false,
-    },
   },
 });
